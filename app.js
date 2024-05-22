a0e44ea481353bfdb7d4c908f734c6776088dd42
Jose Germán Martínez <jgermanmm@gmail.com>
2024-05-22 10:17:49 -0400

    Add express dependency and update app.js to handle chat requests

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 require("dotenv").config();
 
+const express = require("express")
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.KEY);
 
-async function run() {
+async function run(prompt) {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
-
-  const prompt = "Bien, me ayuda con la creación de contenido para mi página web?"
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
-  console.log(text);
+  return text;
 }
 
-run();
+app.post("/chat", async (req, res) => {
+  const { prompt } = req.body;
+  const response = await run(prompt);
+  res.send(`${response}`);
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000")
+})
